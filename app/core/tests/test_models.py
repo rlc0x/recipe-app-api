c75f5f4c624d5f4b9d7f5c9284afd16e309e4f2e
Raymond Cox <rcox@unixandmore.com>
2025-07-05 11:23:25 -0600

    Saving status through 110

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
-def crate_user(email='user@example.com', password='testpass123'):
+def create_user(email='user@example.com', password='testpass123'):
     """Create and return a new user."""
     return get_user_model().objects.create_user(email, password)
 
@@ -69,7 +69,15 @@ class ModelTests(TestCase):
 
     def test_create_tag(self):
         """Test creating a tag is successful"""
-        user = crate_user()
+        user = create_user()
         tag = models.Tag.objects.create(user=user, name='Tag1')
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creatig an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+                user=user,
+                name="Ingredient"
+        )
